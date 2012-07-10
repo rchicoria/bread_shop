629c2aa14e807c8ca51d4c5b5663bd2981e8aa3f
Rui Chicória <rchicoria@gmail.com>
2012-07-10 21:33:42 +0100

    heroku ready

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BreadShop::Application.config.secret_token = 'e965e81ccfad8738fde35eb82abcd901a61b53aed387e5199f40fdf8b77795916cb9caa7e6f61db2c25bfbd7874d75c033b73dbe1294872e4e2c18dc6bfd6813'
